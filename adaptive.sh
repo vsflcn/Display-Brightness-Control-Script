7aa407b23687451199ab50dfd58252dac19c971c
Volha Sakalouskaya <157295066+vsflcn@users.noreply.github.com>
2024-06-16 18:55:37 +0000

    Update adaptive.sh

--- adaptive.sh
+++ adaptive.sh
@@ -6,30 +6,33 @@ set_brightness_mac() {
     osascript -e "tell application "\System Events\" to set brightness level of display 1 to $brighness_level
 }
 
-#Function to manage brightness on Linux
+# Function to manage brightness on Linux
 set_brightness_linux() {
-    local brightness level=$1
-    current_hour=$(date + "%H")
-    if [ current_hour -gt 8 ] && [ current_hour -lt 21 ]
-    then
-        xrandr --output eDP-1 --brightness $brighness_level
+    local brightness_level=$1
+    current_hour=$(date +%H)
+    if [ $current_hour -gt 8 ] && [ $current_hour -lt 20 ]; then
+        xrandr --output eDP-1 --brightness $brightness_level
     else
         xrandr --output eDP-1 --brightness 0.3
     fi
 }
 
-#Function to manage brightness on Windows
-set_brightness_linux() {
-    local brightness level=$1
-    powershell.exe -ExecutionPolicy Bypass -Command "& {Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods | foreach { $_.WmiSetBrightness(1, $brightness_level) }}"
+# Function to manage brightness on Windows
+set_brightness_windows() {
+    local brightness_level=$1
+    powershell.exe -ExecutionPolicy Bypass -Command "
+    \$brightness = $brightness_level
+    \$brightnessMethods = Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods
+    \$brightnessMethods.WmiSetBrightness(1, \$brightness)
+    "
 }
 
-#Check for current operation system
+# Check for current operating system
 case "$(uname -s)" in
     Darwin)
         # macOS
         echo "Running on macOS"
-        set_brightness_macos 0.8  # Set brightness to 80%
+        set_brightness_mac 0.8  # Set brightness to 80%
         ;;
     Linux)
         # Linux
@@ -42,7 +45,7 @@ case "$(uname -s)" in
         set_brightness_windows 50  # Set brightness to 50%
         ;;
     *)
-        # Another operation systems
+        # Another operating systems
         echo "Unsupported operating system"
         exit 1
         ;;
