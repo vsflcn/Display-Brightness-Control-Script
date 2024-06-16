9741060e7d902ac595aed24c3921fca496c3668e
vsflcn <falcoooneee@proton.me>
2024-06-16 16:58:19 +0000

    Function to manage brighness on MacOS

--- adaptive.sh
+++ adaptive.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+#Function to manage brighness on MacOS
+set_brightness_mac() {
+    local brighness_level=$1
+    osascript -e "tell application "\System Events\" to set brightness level of display 1 to $brighness_level
+}
